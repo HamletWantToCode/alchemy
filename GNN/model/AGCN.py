54b299582090175a6500df1c703af9fb267f5b31
hongbinren <hongbinrenscu@outlook.com>
2019-07-22 22:39:07 +0800

    add mas metric

--- GNN/model/AGCN.py
+++ GNN/model/AGCN.py
@@ -1,6 +1,6 @@
 import torch
 from ..nn import SGC_LL, graph_max_pool
-from torch_geometric.nn import global_add_pool
+from torch_geometric.nn import global_add_pool #, Set2Set
 from torch.nn import BatchNorm1d
 import torch.nn.functional as F
 
@@ -13,11 +13,12 @@ class AGCN(torch.nn.Module):
                  K=4,
                  alpha=0.5,
                  num_step_combo=2,
+                #  num_step_set2set=6,
                  filter_in_channel=1,
                  filter_out_channel=1,
                  filer_kernel_size=3,
                  fcc1_hidden_dim=50,
-                 fcc2_hidden_dim=20,
+                 fcc2_hidden_dim=15,
                  output_dim=12,
                  ):
 
@@ -26,7 +27,7 @@ class AGCN(torch.nn.Module):
         self.num_step_combo = num_step_combo
         self.conv = SGC_LL(node_input_dim, node_hidden_dim, K, alpha)
         self.batch_norm = BatchNorm1d(node_hidden_dim)
-
+        # self.set2set = Set2Set(node_hidden_dim, processing_steps=num_step_set2set)
         self.filter = torch.nn.Conv1d(filter_in_channel, filter_out_channel, filer_kernel_size, padding=1)
         self.lin1 = torch.nn.Linear(node_hidden_dim, fcc1_hidden_dim)
         self.lin2 = torch.nn.Linear(fcc1_hidden_dim, fcc2_hidden_dim)
@@ -44,6 +45,7 @@ class AGCN(torch.nn.Module):
             out = graph_max_pool(out, edge_index)
 
         out = global_add_pool(out, batch)
+        # out = self.set2set(out, batch)
         out = (self.filter(out.unsqueeze(dim=1))).squeeze(dim=1)
         
         out = F.relu(self.lin1(out))
--- GNN/model/metric.py
+++ GNN/model/metric.py
@@ -6,3 +6,9 @@ def mse_metric(output, target):
         assert output.shape[0] == len(target)
         accuracy = torch.mean((output - target)**2, dim=0).numpy()
     return accuracy
+
+def mae_metric(output, target):
+    with torch.no_grad():
+        assert output.shape[0] == len(target)
+        accuracy = torch.mean(torch.mean(torch.abs(output-target), dim=0)).numpy()
+    return accuracy
